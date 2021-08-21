81dcadd90a7add675b442e9e30ee1be688c8c472
daniel <dap5@cin.ufpe.br>
2021-08-21 18:04:31 -0300

    add file roteiro.ts with the class Roteiro to common

---
+++ common/roteiros.ts
@@ -0,0 +1,24 @@
+export class Roteiro {
+    nome: string;
+    dataDeEntrega: "";
+  
+    constructor() {
+      this.clean();
+    }
+  
+    clean(): void {
+      this.nome = "";
+      this.dataDeEntrega = "";
+    }
+  
+    clone(): Roteiro {
+      var roteiro: Roteiro = new Roteiro();
+      roteiro.copyFrom(this);
+      return roteiro;
+    }
+  
+    copyFrom(from: Roteiro): void {
+      this.nome = from.nome;
+      this.dataDeEntrega = from.dataDeEntrega;
+    }  
+}
